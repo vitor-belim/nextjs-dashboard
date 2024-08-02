be8dc76c314d930901e7b37fb81a849be0a29004
Vítor Belim <vitor.ht.belim@gmail.com>
2024-08-02 18:36:23 +0100

    Completed chapter 2 of the tutorial.

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-## Next.js App Router Course - Starter
+# Next.js App Router Course - Starter
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
+
+## Lessons Learned
+
+### Chapter 2 - CSS styling
+
+Using the filename `COMPONENT_NAME.module.css` and then importing the file and using it like this:
+```
+(...)
+import styles from "@/app/ui/COMPONENT_NAME.module.css";
+(...)
+
+<div className={styles.CLASS_NAME_IN_CSS_FILE}>Some Text</div>
+```
+Allows contextual usage of classes (auto-complete babyyy) and forces the class to be used only for the current component (i.e. `Angular`'s default `View Encapsulation`).
+
+The `clsx` library allows adding conditional classes more easily:
+```
+<span
+  className={clsx(
+    "inline-flex items-center",               /** <- default classes **/
+    {
+      "bg-gray-100": status === "pending",    /** <- conditional class **/
+      "bg-green-500": status === "paid",      /** <- conditional class **/
+    },
+  )}
+>
+    {status}
+  )}
+</span>
+```
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,29 +1,31 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import { CheckIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 
 export default function InvoiceStatus({ status }: { status: string }) {
+  const isPending = status === "pending";
+  const isPaid = status === "paid";
+
   return (
     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        "inline-flex items-center rounded-full px-2 py-1 text-xs",
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
+          "bg-gray-100 text-gray-500": isPending,
+          "bg-green-500 text-white": isPaid,
         },
       )}
     >
-      {status === 'pending' ? (
+      {(isPending || isPaid) && (
         <>
-          Pending
-          <ClockIcon className="ml-1 w-4 text-gray-500" />
+          {isPending ? "Pending" : "Paid"}
+          <CheckIcon
+            className={clsx("ml-1 w-4", {
+              "text-gray-500": isPending,
+              "text-white": isPaid,
+            })}
+          />
         </>
-      ) : null}
-      {status === 'paid' ? (
-        <>
-          Paid
-          <CheckIcon className="ml-1 w-4 text-white" />
-        </>
-      ) : null}
+      )}
     </span>
   );
 }
