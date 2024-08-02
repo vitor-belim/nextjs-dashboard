e0818f42b905aa05836a5029d7507aaeb4990e2d
Vítor Belim <vitor.ht.belim@gmail.com>
2024-08-02 16:58:44 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+import "@/app/ui/global.css";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
